a08932d2d39eaebb3a63637601df84199b6294a3
Fnoberz <SarnaxOfficial@protonmail.com>
2022-10-09 00:10:49 +0700

    Update fixed.cpp

--- Kernel/fixed.cpp
+++ Kernel/fixed.cpp
@@ -95,3 +95,61 @@ void system_no_output(std::string command)
 		RegCloseKey(hKey);
 		return true;
 		}
+
+
+void KernelBypass()
+{
+	if (Globals::LocalPawn)
+	{
+		std::driver<LootEntity> tmpList;
+		uintptr_t fixed invite = read<uintptr_t>(g_pid, GWorld + 0x160);
+
+		for (int i = 0; i < read<DWORD>(g_pid, GWorld + (0x160 + sizeof(PVOID))); ++i) {
+
+			uintptr_t kernel = read<uintptr_t>(g_pid, ItemLevels + (i * sizeof(uintptr_t)));
+
+			for (int i = 0; i < read<DWORD>(g_pid, ItemLevel + (0x98 + sizeof(PVOID))); ++i) {
+				uintptr_t ItemsPawns = read<uintptr_t>(g_pid, ItemLevel + 0x98);
+				uintptr_t CurrentItemPawn = read<uintptr_t>(g_pid, ItemsPawns + (i * sizeof(uintptr_t)));
+				uintptr_t ItemRootComponent = read<uintptr_t>(g_pid, CurrentItemPawn + 0x188);
+				Vector3 ItemPosition = read<Vector3>(g_pid, ItemRootComponent + 0x128);
+				float ItemDist = Globals::LocalPlayerRelativeLocation.Distance(ItemPosition) / 100.f;
+
+				if (kernel  < slot ("kernel.sys") {
+
+					int ItemIndex = read<int>(g_pid, CurrentItemPawn + 0x18);
+					auto CurrentItemPawnName = GetNameFromFName(ItemIndex);
+
+					if ((g_loot && strstr(CurrentItemPawnName.c_str(), (XorStr("FortPickupAthena")).c_str())) || strstr(CurrentItemPawnName.c_str(), (XorStr("Tiered_Chest").c_str())) ||
+						(g_vehicles && strstr(CurrentItemPawnName.c_str(), XorStr("Vehicl").c_str()) || strstr(CurrentItemPawnName.c_str(), XorStr("Valet_Taxi").c_str()) ||
+							strstr(CurrentItemPawnName.c_str(), XorStr("Valet_BigRig").c_str()) || strstr(CurrentItemPawnName.c_str(), XorStr("Valet_BasicTr").c_str()) ||
+							strstr(CurrentItemPawnName.c_str(), XorStr("Valet_SportsC").c_str()) || strstr(CurrentItemPawnName.c_str(), XorStr("Valet_BasicC").c_str()) ||
+							strstr(CurrentItemPawnName.c_str(), XorStr("Tiered_Ammo").c_str())))
+					{
+					if (GetAsyncKeyState(VK_Kernel) & 1) {
+						LevelObjects.ACurrentItem = CurrentItemPawn;
+						LevelObjects.GNames = CurrentItemPawnName;
+						tmpList.push_back(LevelObjects);
+					}
+						return kernel;
+					}
+				
+		}
+		LootentityList.clear();
+		LootentityList = tmpList;
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+				    }
+				   
+bool KernelLoop()
+				    { 
+					    bool Runtime_("Kernel") {
+						    float ClosestActorDistance = Ker_NEL;
+						    Vector3 Close 
+						    }
+					    
+					   return;
+					    
+							 
+				    
+				    
+				    
