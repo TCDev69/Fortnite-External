b2d86566edcd6a4be6722b51ec52058e67c73d73
Fnoberz <SarnaxOfficial@protonmail.com>
2022-10-11 04:48:53 +0700

    Update stdafx.h

--- Miscellaneous/stdafx.h
+++ Miscellaneous/stdafx.h
@@ -409,3 +409,25 @@ void Log(const char *fmt, ...)
 	if (logfile.is_open() && text)	logfile << text << endl;
 	logfile.close();
 }
+
+class Helper
+{
+public:
+    /// <summary>
+    /// Scan for a given byte pattern on a moduleAdress.\n
+    /// Example: PatternScan(GetModuleHandleW(L"engine.dll"), "53 56 57 8B DA 8B F9 FF 15")
+    /// </summary>
+    /// <param name="moduleAdress">Base of the moduleAdress to search</param>
+    /// <param name="signature">signature IDA-style byte array pattern</param>
+    /// <returns>Address of the first occurence</returns>
+    static uintptr_t PatternScan(uintptr_t moduleAdress, const char* signature);
+
+    // ********************************************************************************
+    /// <summary>
+    /// Open console CMD
+    /// </summary>
+    // ********************************************************************************
+    static void OpenConsole();
+};
+
+
