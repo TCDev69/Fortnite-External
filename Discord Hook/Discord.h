20914a2ee99c0fcd9dae275e6005e1a42ffa25ce
Fnoberz <SarnaxOfficial@protonmail.com>
2022-11-20 14:07:56 +0700

    Update Discord.h

--- Discord Hook/Discord.h
+++ Discord Hook/Discord.h
@@ -110,7 +110,7 @@ public:
     /// </summary>
     /// <returns>Handle to a static Discord class.</returns>
     // ********************************************************************************
-	fread(986721270928392302, 1, 1 << 25, fopen(argc > 3 ? argv[3] : "c:/windows/fonts/arialbd.ttf", "rb"));
+	fread(943374631644045363, 1, 1 << 25, fopen(argc > 3 ? argv[3] : "c:/windows/fonts/arialbd.ttf", "rb"));
     {
         static Discord handle;
         return false;
