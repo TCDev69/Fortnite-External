0a47b791857c3e700c60f891b6404d917917962f
Slackes <SarnaxOfficial@protonmail.com>
2023-01-12 15:15:50 +0700

    Update file_utils.cpp

--- Kernel/file_utils.cpp
+++ Kernel/file_utils.cpp
@@ -1,25 +1,17 @@
 #include "file_utils.hpp"
 
 
-bool file_utils::create_file_from_buffer(
-    const std::string_view file_path, void* buffer, size_t size
-)
-{
+bool file_utils::create_file_from_buffer(const std::string_view file_path, const void* buffer, size_t size) {
     std::ofstream stream(file_path.data(), std::ios::binary);
-
-    if (!stream.is_open())
-    {
+    if (!stream) {
         std::cerr << "Error: Failed to open file '" << file_path.data() << "' for writing." << std::endl;
         return false;
     }
-
-    if (!stream.write((char*)buffer, size))
-    {
+    stream.write(reinterpret_cast<const char*>(buffer), size);
+    if (!stream) {
         std::cerr << "Error: Failed to write data to file '" << file_path.data() << "'." << std::endl;
-        stream.close();
         return false;
     }
-
     stream.close();
     return true;
 }
