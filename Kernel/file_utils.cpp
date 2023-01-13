2d0837a76ca63318ec369b35c74e8888dba3db6f
Developer <81179408+RutOf@users.noreply.github.com>
2023-01-13 14:28:47 +0700

    Update file_utils.cpp

--- Kernel/file_utils.cpp
+++ Kernel/file_utils.cpp
@@ -2,14 +2,16 @@
 
 
 bool file_utils::create_file_from_buffer(const std::string_view file_path, const void* buffer, size_t size) {
-    std::ofstream stream(file_path.data(), std::ios::binary);
+    std::ofstream stream(file_path.data(), std::ios::binary | std::ios::trunc); 
     if (!stream) {
         std::cerr << "Error: Failed to open file '" << file_path.data() << "' for writing." << std::endl;
         return false;
     }
-    stream.write(reinterpret_cast<const char*>(buffer), size);
-    if (!stream) {
-        std::cerr << "Error: Failed to write data to file '" << file_path.data() << "'." << std::endl;
+    stream.exceptions(std::ios::failbit | std::ios::badbit);
+    try {
+        stream.write(reinterpret_cast<const char*>(buffer), size);
+    } catch (const std::ofstream::failure& e) {
+        std::cerr << "Error: Failed to write data to file '" << file_path.data() << "': " << e.what() << std::endl;
         return false;
     }
     stream.close();
