af584b7200228f45f00e3ecb0171fd2e5fa28b78
Slackes <SarnaxOfficial@protonmail.com>
2023-01-29 03:54:24 +0700

    It is also a good practice to use log library to print errors instead of std::cerr

--- Kernel/file_utils.cpp
+++ Kernel/file_utils.cpp
@@ -2,18 +2,19 @@
 
 
 bool file_utils::create_file_from_buffer(const std::string_view file_path, const void* buffer, size_t size) {
-    std::ofstream stream(file_path.data(), std::ios::binary | std::ios::trunc); 
-    if (!stream) {
-        std::cerr << "Error: Failed to open file '" << file_path.data() << "' for writing." << std::endl;
-        return false;
-    }
-    stream.exceptions(std::ios::failbit | std::ios::badbit);
     try {
-        stream.write(reinterpret_cast<const char*>(buffer), size);
+        std::ofstream stream(file_path.data(), std::ios::binary | std::ios::trunc); 
+        if (!stream) {
+            log::error("Failed to open file {} for writing.", file_path.data());
+            return false;
+        }
+        stream.exceptions(std::ios::failbit | std::ios::badbit);
+
+        stream.write(reinterpret_cast<const char*>(buffer), size
+                     
     } catch (const std::ofstream::failure& e) {
         std::cerr << "Error: Failed to write data to file '" << file_path.data() << "': " << e.what() << std::endl;
         return false;
     }
-    stream.close();
     return true;
 }
