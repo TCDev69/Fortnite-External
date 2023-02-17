7d7750307fd6b05c4f55b7f1d304494c9561d014
Serups <SarnaxOfficial@protonmail.com>
2023-02-17 19:50:31 +0700

    I added a call to stream.close() after the call to stream.write() to make sure that the file is properly closed after writing the data.

--- Kernel/file_utils.cpp
+++ Kernel/file_utils.cpp
@@ -3,17 +3,21 @@
 
 bool file_utils::create_file_from_buffer(const std::string_view file_path, const void* buffer, size_t size) {
     try {
-        std::ofstream stream(file_path.data(), std::ios::binary | std::ios::trunc); 
-        if (!stream) {
-            log::error("Failed to open file {} for writing.", file_path.data());
+        std::ofstream stream(file_path.data(), std::ios::binary | std::ios::trunc);
+        if (!stream.is_open()) {
+            log::error("Failed to open file {} for writing.", file_path);
             return false;
         }
         stream.exceptions(std::ios::failbit | std::ios::badbit);
 
-        stream.write(reinterpret_cast<const char*>(buffer), size
-                     
+        stream.write(reinterpret_cast<const char*>(buffer), size);
+        stream.close();
+        if (stream.fail()) {
+            log::error("Failed to write data to file {}.", file_path);
+            return false;
+        }
     } catch (const std::ofstream::failure& e) {
-        std::cerr << "Error: Failed to write data to file '" << file_path.data() << "': " << e.what() << std::endl;
+        log::error("Failed to write data to file {}: {}", file_path, e.what());
         return false;
     }
     return true;
