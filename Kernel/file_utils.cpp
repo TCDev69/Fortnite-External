42534d5880bb7263ae04d439d6609011b1c3b0ec
Serups <SarnaxOfficial@protonmail.com>
2023-02-26 00:24:27 +0700

    Create file from buffer.

--- Kernel/file_utils.cpp
+++ Kernel/file_utils.cpp
@@ -4,21 +4,28 @@
 bool file_utils::create_file_from_buffer(const std::string_view file_path, const void* buffer, size_t size) {
     try {
         std::ofstream stream(file_path.data(), std::ios::binary | std::ios::trunc);
-        if (!stream.is_open()) {
+        if (!stream) {
             log::error("Failed to open file {} for writing.", file_path);
             return false;
         }
-        stream.exceptions(std::ios::failbit | std::ios::badbit);
 
         stream.write(reinterpret_cast<const char*>(buffer), size);
-        stream.close();
-        if (stream.fail()) {
-            log::error("Failed to write data to file {}.", file_path);
+
+        if (stream.bad()) {
+            log::error("Failed to write data to file {} due to a bad stream state.", file_path);
             return false;
         }
+
+        if (stream.fail()) {
+            log::error("Failed to write data to file {} due to a write failure.", file_path);
+            return false;
+        }
+
+        stream.close();
     } catch (const std::ofstream::failure& e) {
         log::error("Failed to write data to file {}: {}", file_path, e.what());
         return false;
     }
+
     return true;
 }
