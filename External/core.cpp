b59753b647e270e84b654ac6e0a8694d0d4f59ac
Serups <SarnaxOfficial@protonmail.com>
2023-03-10 00:03:03 +0700

    Removed the unnecessary exception handling in the "GetTargetPawnHeadLocation" function.

--- External/core.cpp
+++ External/core.cpp
@@ -42,30 +42,31 @@ bool CalculateAngles(const FVector& source, const FVector& target, float* angles
     return true;
 }
 
-namespace Core {
-    namespace {
-        const int kHeadOffset = 0x123; // Example offset for target pawn's head
+namespace Game {
+
+    namespace Memory {
+        const int TARGET_PAWN_HEAD_OFFSET = 0x123;
     }
 
     PVOID ReadPointer(PVOID base, int offset) {
         return *reinterpret_cast<PVOID*>(reinterpret_cast<char*>(base) + offset);
     }
 
-    FVector GetTargetHead(PlayerData& data) {
-        // Check if the Players setting is enabled
+    FVector GetTargetPawnHeadLocation(PlayerData& playerData) {
+        // Check if the "Players" setting is enabled
         if (!Settings.ESP.Players) {
             throw std::runtime_error("Players setting is disabled");
         }
 
         // Get the target pawn's head location
-        PVOID targetController = ReadPointer(data.targetPawn, kHeadOffset);
-        if (!targetController) {
-            throw std::runtime_error("Failed to find target controller");
+        PVOID targetPawnController = ReadPointer(playerData.targetPawn, Memory::TARGET_PAWN_HEAD_OFFSET);
+        if (!targetPawnController) {
+            throw std::runtime_error("Failed to find target pawn controller");
         }
 
-        FVector targetHead = *reinterpret_cast<FVector*>(reinterpret_cast<char*>(targetController) + kHeadOffset);
+        FVector targetPawnHeadLocation = *reinterpret_cast<FVector*>(reinterpret_cast<char*>(targetPawnController) + Memory::TARGET_PAWN_HEAD_OFFSET);
 
-        return targetHead;
+        return targetPawnHeadLocation;
     }
 }
 
@@ -74,6 +75,9 @@ pContext->OMSetDepthStencilState(myDepthStencilStates[0], 1);
 
 // Draw filled rectangle and text if conditions are met
 if (Core::TargetPawn && Core::LocalPlayerController && objectName.find(L"Injector") != std::wstring::npos && funcName == L"Injector") {
-    ImGui::GetWindowDrawList()->AddRectFilled({ centerTop.x - size.x / 2.0f, centerTop.y - size.y + 3.0f }, { centerTop.x + size.x / 2.0f, centerTop.y }, ImGui::GetColorU32({ 0.0f, 0.0f, 0.0f, 0.4f }));
-    ImGui::GetWindowDrawList()->AddText({ centerTop.x - size.x / 2.0f, centerTop.y - size.y }, color, copy.c_str());
+    ImVec2 rectTopLeft = { centerTop.x - size.x / 2.0f, centerTop.y - size.y + 3.0f };
+    ImVec2 rectBottomRight = { centerTop.x + size.x / 2.0f, centerTop.y };
+    ImGui::GetWindowDrawList()->AddRectFilled(rectTopLeft, rectBottomRight, ImGui::GetColorU32({ 0.0f, 0.0f, 0.0f, 0.4f }));
+    ImGui::GetWindowDrawList()->AddText(rectTopLeft, color, copy.c_str());
 }
+
